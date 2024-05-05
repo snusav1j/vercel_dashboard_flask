37b43be9a9d2dd4047207760c78886db08503239
samir <106090175+snusav1j@users.noreply.github.com>
2024-05-05 09:14:19 +0300

    Update get_wallet_balance.js

--- static/scripts/get_wallet_balance.js
+++ static/scripts/get_wallet_balance.js
@@ -6,7 +6,7 @@ setInterval(function() {
             get_wallet_info(res);
         }
     });
-}, 2000)
+}, 3000)
 
 function get_wallet_info(res){
     // данные с байбита
@@ -38,9 +38,46 @@ function get_wallet_info(res){
     $('.pie-chart').css("background",`conic-gradient(var(--crypto) 0% ${String(total_crypto_percent)}%, var(--case) ${String(total_crypto_percent)}% 100%)`);
     
     // засунуть полученные данные в html
+    setTimeout(function() {
+        $('.crypto .balance-value').attr('data-last-val', Number(total_crypto_balance).toFixed(2));
+        $('.case .balance-value').attr('data-last-val', Number(total_case_balance).toFixed(2));
+        $('.money .balance-value').attr('data-last-val', Number(total_wallet_balance).toFixed(2));
+    }, 100)
+
+    // код изменения активов в сравнении с предыдущим (упростить код)
+    last_crypto_val = Number($('.crypto .balance-value').attr('data-last-val'))
+    now_crypto_val = total_crypto_balance
+    last_case_val = Number($('.case .balance-value').attr('data-last-val'))
+    now_case_val = total_case_balance
+    last_money_val = Number($('.money .balance-value').attr('data-last-val'))
+    now_money_val = total_wallet_balance
+
+    if (last_crypto_val < now_crypto_val){
+        $('.crypto .balance-info .curr-up').removeClass('hide')
+        $('.crypto .balance-info .curr-down').addClass('hide')
+    }else{
+        $('.crypto .balance-info .curr-up').addClass('hide')
+        $('.crypto .balance-info .curr-down').removeClass('hide')
+    }
+    if (last_case_val < now_case_val){
+        $('.case .balance-info .curr-up').removeClass('hide')
+        $('.case .balance-info .curr-down').addClass('hide')
+    }else{
+        $('.case .balance-info .curr-up').addClass('hide')
+        $('.case .balance-info .curr-down').removeClass('hide')
+    }
+    if (last_money_val < now_money_val){
+        $('.money .balance-info .curr-up').removeClass('hide')
+        $('.money .balance-info .curr-down').addClass('hide')
+    }else{
+        $('.money .balance-info .curr-up').addClass('hide')
+        $('.money .balance-info .curr-down').removeClass('hide')
+    }
+
+    //  конвертировать валюту
     if ($('.curr-convert > input').is(':checked') == false){
-        $('.case .balance-value').text(`$${Number(total_case_balance).toFixed(2)}`) 
         $('.crypto .balance-value').text(`$${Number(total_crypto_balance).toFixed(2)}`)
+        $('.case .balance-value').text(`$${Number(total_case_balance).toFixed(2)}`)
         $('.money .balance-value').text(`$${Number(total_wallet_balance).toFixed(2)}`)
         $('.curr-convert > label').text('USD')
     }else{
