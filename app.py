26ac97f3729d4dd60c84ae0108451f42fced1f4c
samir <106090175+snusav1j@users.noreply.github.com>
2024-05-05 15:07:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ from keys import apiKey, secretKey
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def dashboard():
+    return render_template("dashboard.html")
+@app.route('/crypto.html')
+def crypto():
+    return render_template("crypto.html")
+@app.route('/case.html')
+def case():
+    return render_template("case.html")
+@app.route('/money.html')
+def money():
+    return render_template("money.html")
 
 @app.route('/wallet_info', methods=["GET"])
 def get_wallet_info():
@@ -21,7 +30,10 @@ def get_wallet_info():
 
     wallet_info = []
     for i in range(len(result['result']['list'][0]['coin'])):
-        wallet_info.append({'coinName': result['result']['list'][0]['coin'][i]['coin'], 'coinQuanity': result['result']['list'][0]['coin'][i]['equity'], 'usdValue': result['result']['list'][0]['coin'][i]['usdValue']})
+        coin_name = result['result']['list'][0]['coin'][i]['coin']
+        coin_quanity = result['result']['list'][0]['coin'][i]['equity']
+        usd_value = result['result']['list'][0]['coin'][i]['usdValue']
+        wallet_info.append({'coinName': coin_name, 'coinQuanity': coin_quanity, 'usdValue': usd_value})
         
     wallet_info.insert(0, {'walletBalance': result['result']['list'][0]['totalEquity']})
     return wallet_info
